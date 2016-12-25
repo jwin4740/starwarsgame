2f9064e7b0c077052977d849f5a8de84c8e4969f
James <jwin4740@gmail.com>
2016-12-25 12:41:11 -0500

    christmas

--- assets/javascript/rogue.js
+++ assets/javascript/rogue.js
@@ -6,6 +6,8 @@ var attacker;
 var currentdefender;
 var defender1, defender2, defender3;
 var checkpoint1 = false;
+var checkpoint2 = false;
+var tempval;
 
 
 
@@ -17,19 +19,19 @@ $(document).ready(function() {
 
 
     // prototype object
-    function Character(name, hP, aP, cAP, id, nameheader, imagesource) {
+    function Character(name, hP, aP, cAP, id, num, nameheader, imagesource) {
         this.name = name;
         this.hP = hP;
         this.aP = aP;
         this.cAP = cAP;
-        this.ele = '<div ' + id + 'class="player"><h4>' + nameheader + '</h4><img class="pimage" src="assets/images/' + imagesource + '</div>';
+        this.ele = '<div ' + id + 'class="player" data-value="' + num + '"><h4>' + nameheader + '</h4><img class="pimage" src="assets/images/' + imagesource + '</div>';
     };
 
 
-    var charOne = new Character("ObiwanKenobi", 175, 12, 15, 'id="obi" ', 'Obi-wan Kenobi', 'obiwan.jpg">');
-    var charTwo = new Character("QuigonGinn", 200, 14, 20, 'id="qui" ', 'Qui-gon Ginn', 'quigon.jpeg">');
-    var charThree = new Character("CountDooku", 250, 16, 25, 'id="count" ', 'Count Dooku', 'countdooku.jpg">');
-    var charFour = new Character("DarthMaul", 150, 18, 30, 'id="darth" ', 'Darth Maul', 'darthmaul.jpeg">');
+    var charOne = new Character("ObiwanKenobi", 175, 12, 15, 'id="obi" ', 1, 'Obi-wan Kenobi', 'obiwan.jpg">');
+    var charTwo = new Character("QuigonGinn", 200, 14, 20, 'id="qui" ', 2, 'Qui-gon Ginn', 'quigon.jpeg">');
+    var charThree = new Character("CountDooku", 250, 16, 25, 'id="count" ', 3, 'Count Dooku', 'countdooku.jpg">');
+    var charFour = new Character("DarthMaul", 150, 18, 30, 'id="darth" ', 4, 'Darth Maul', 'darthmaul.jpeg">');
 
     //fill character array
     cArray.push(charOne);
@@ -44,30 +46,34 @@ $(document).ready(function() {
     // user chooses their player which appends to the attacker box, while the remaining players get appended
     // to the defenderondeck box
     function setup() {
-        $("#choosebox").html(charOne.ele);
-        $("#choosebox").append(charTwo.ele);
-        $("#choosebox").append(charThree.ele);
-        $("#choosebox").append(charFour.ele);
+        $("#choosebox").append(charOne.ele).append(charTwo.ele).append(charThree.ele).append(charFour.ele);
     }
 
 
-setup();
+    setup();
 
     $('.player').on("click", function() {
-        if (checkpoint1 === false) {
+        if (!checkpoint1) {
             $(this).siblings().addClass("defender").appendTo("#defenderondeckbox");
             $(this).addClass("attacker").appendTo("#attackerbox");
-            console.log(checkpoint1);
+
+            tempval = $(this).attr("data-value");
+            attacker = cArray[tempval - 1];
+            console.log(attacker);
             checkpoint1 = true;
 
         }
     });
 
     $("#defenderondeckbox").on("click", ".player.defender", function() {
-        
-        currentdefender = this;
-        console.log(currentdefender);
-        $(currentdefender).appendTo("#defenderbox");
+        if (!checkpoint2) {
+            $(this).appendTo("#defenderbox");
+            tempval = $(this).attr("data-value");
+            currentdefender = cArray[tempval - 1];
+            console.log(currentdefender);
+            checkpoint2 = true;
+            $("#defenderbox").append();
+        }
     });
 
 
